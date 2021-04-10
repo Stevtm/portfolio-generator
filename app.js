4d09e655e28d7dbb8e1f44a6aca2cae5cd1d518c
Stevtm <steventmaddison@outlook.com>
2021-04-09 20:13:09 -0400

    generate html file using js fs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const profileDataArr = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
-console.log(profileDataArr);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = (profileDataArr) => {
-	profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArr);
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+	if (err) throw err;
+
+	console.log("Portfolio complete! Check out index.html to see the output!");
+});
